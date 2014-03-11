ee0e50a293e390cd0a5bf0cc7bfe099b2bb4f729
Josh Kugelmann <joshk@y7mail.com>
2014-03-11 22:38:31 +1100

    This is shameful.

---
+++ JumpControl/Main.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.ShutDownCheckBox = New System.Windows.Forms.CheckBox()
+        Me.RestartCheckBox = New System.Windows.Forms.CheckBox()
+        Me.SleepCheckBox = New System.Windows.Forms.CheckBox()
+        Me.SwitchUserCheckBox = New System.Windows.Forms.CheckBox()
+        Me.LockCheckBox = New System.Windows.Forms.CheckBox()
+        Me.LogOffCheckBox = New System.Windows.Forms.CheckBox()
+        Me.AuthorLinkLabel = New System.Windows.Forms.LinkLabel()
+        Me.AuthorLabel = New System.Windows.Forms.Label()
+        Me.ChooseItemsLabel = New System.Windows.Forms.Label()
+        Me.LogoPictureBox = New System.Windows.Forms.PictureBox()
+        Me.RestartExplorerCheckBox = New System.Windows.Forms.CheckBox()
+        CType(Me.LogoPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ShutDownCheckBox
+        '
+        Me.ShutDownCheckBox.AutoSize = True
+        Me.ShutDownCheckBox.Location = New System.Drawing.Point(12, 31)
+        Me.ShutDownCheckBox.Name = "ShutDownCheckBox"
+        Me.ShutDownCheckBox.Size = New System.Drawing.Size(79, 17)
+        Me.ShutDownCheckBox.TabIndex = 0
+        Me.ShutDownCheckBox.Text = "Shut Down"
+        Me.ShutDownCheckBox.UseVisualStyleBackColor = True
+        '
+        'RestartCheckBox
+        '
+        Me.RestartCheckBox.AutoSize = True
+        Me.RestartCheckBox.Location = New System.Drawing.Point(12, 54)
+        Me.RestartCheckBox.Name = "RestartCheckBox"
+        Me.RestartCheckBox.Size = New System.Drawing.Size(60, 17)
+        Me.RestartCheckBox.TabIndex = 0
+        Me.RestartCheckBox.Text = "Restart"
+        Me.RestartCheckBox.UseVisualStyleBackColor = True
+        '
+        'SleepCheckBox
+        '
+        Me.SleepCheckBox.AutoSize = True
+        Me.SleepCheckBox.Location = New System.Drawing.Point(12, 77)
+        Me.SleepCheckBox.Name = "SleepCheckBox"
+        Me.SleepCheckBox.Size = New System.Drawing.Size(53, 17)
+        Me.SleepCheckBox.TabIndex = 0
+        Me.SleepCheckBox.Text = "Sleep"
+        Me.SleepCheckBox.UseVisualStyleBackColor = True
+        '
+        'SwitchUserCheckBox
+        '
+        Me.SwitchUserCheckBox.AutoSize = True
+        Me.SwitchUserCheckBox.Location = New System.Drawing.Point(12, 100)
+        Me.SwitchUserCheckBox.Name = "SwitchUserCheckBox"
+        Me.SwitchUserCheckBox.Size = New System.Drawing.Size(83, 17)
+        Me.SwitchUserCheckBox.TabIndex = 0
+        Me.SwitchUserCheckBox.Text = "Switch User"
+        Me.SwitchUserCheckBox.UseVisualStyleBackColor = True
+        '
+        'LockCheckBox
+        '
+        Me.LockCheckBox.AutoSize = True
+        Me.LockCheckBox.Location = New System.Drawing.Point(12, 123)
+        Me.LockCheckBox.Name = "LockCheckBox"
+        Me.LockCheckBox.Size = New System.Drawing.Size(50, 17)
+        Me.LockCheckBox.TabIndex = 0
+        Me.LockCheckBox.Text = "Lock"
+        Me.LockCheckBox.UseVisualStyleBackColor = True
+        '
+        'LogOffCheckBox
+        '
+        Me.LogOffCheckBox.AutoSize = True
+        Me.LogOffCheckBox.Location = New System.Drawing.Point(12, 146)
+        Me.LogOffCheckBox.Name = "LogOffCheckBox"
+        Me.LogOffCheckBox.Size = New System.Drawing.Size(61, 17)
+        Me.LogOffCheckBox.TabIndex = 0
+        Me.LogOffCheckBox.Text = "Log Off"
+        Me.LogOffCheckBox.UseVisualStyleBackColor = True
+        '
+        'AuthorLinkLabel
+        '
+        Me.AuthorLinkLabel.AutoSize = True
+        Me.AuthorLinkLabel.Location = New System.Drawing.Point(54, 218)
+        Me.AuthorLinkLabel.Name = "AuthorLinkLabel"
+        Me.AuthorLinkLabel.Size = New System.Drawing.Size(167, 13)
+        Me.AuthorLinkLabel.TabIndex = 2
+        Me.AuthorLinkLabel.TabStop = True
+        Me.AuthorLinkLabel.Text = "http://protosphere.deviantart.com"
+        '
+        'AuthorLabel
+        '
+        Me.AuthorLabel.AutoSize = True
+        Me.AuthorLabel.Location = New System.Drawing.Point(54, 201)
+        Me.AuthorLabel.Name = "AuthorLabel"
+        Me.AuthorLabel.Size = New System.Drawing.Size(78, 13)
+        Me.AuthorLabel.TabIndex = 3
+        Me.AuthorLabel.Text = "© ProtoSphere"
+        '
+        'ChooseItemsLabel
+        '
+        Me.ChooseItemsLabel.AutoSize = True
+        Me.ChooseItemsLabel.Location = New System.Drawing.Point(12, 9)
+        Me.ChooseItemsLabel.Name = "ChooseItemsLabel"
+        Me.ChooseItemsLabel.Size = New System.Drawing.Size(144, 13)
+        Me.ChooseItemsLabel.TabIndex = 4
+        Me.ChooseItemsLabel.Text = "Choose which items to show:"
+        '
+        'LogoPictureBox
+        '
+        Me.LogoPictureBox.BackColor = System.Drawing.Color.Transparent
+        Me.LogoPictureBox.Image = Global.JumpControl.My.Resources.Resources.Logo
+        Me.LogoPictureBox.Location = New System.Drawing.Point(12, 198)
+        Me.LogoPictureBox.Name = "LogoPictureBox"
+        Me.LogoPictureBox.Size = New System.Drawing.Size(36, 36)
+        Me.LogoPictureBox.TabIndex = 1
+        Me.LogoPictureBox.TabStop = False
+        '
+        'RestartExplorerCheckBox
+        '
+        Me.RestartExplorerCheckBox.AutoSize = True
+        Me.RestartExplorerCheckBox.Location = New System.Drawing.Point(12, 169)
+        Me.RestartExplorerCheckBox.Name = "RestartExplorerCheckBox"
+        Me.RestartExplorerCheckBox.Size = New System.Drawing.Size(101, 17)
+        Me.RestartExplorerCheckBox.TabIndex = 0
+        Me.RestartExplorerCheckBox.Text = "Restart Explorer"
+        Me.RestartExplorerCheckBox.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(230, 246)
+        Me.Controls.Add(Me.ChooseItemsLabel)
+        Me.Controls.Add(Me.AuthorLabel)
+        Me.Controls.Add(Me.AuthorLinkLabel)
+        Me.Controls.Add(Me.LogoPictureBox)
+        Me.Controls.Add(Me.RestartExplorerCheckBox)
+        Me.Controls.Add(Me.LogOffCheckBox)
+        Me.Controls.Add(Me.LockCheckBox)
+        Me.Controls.Add(Me.SwitchUserCheckBox)
+        Me.Controls.Add(Me.SleepCheckBox)
+        Me.Controls.Add(Me.RestartCheckBox)
+        Me.Controls.Add(Me.ShutDownCheckBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "MainForm"
+        Me.Text = "JumpControl"
+        CType(Me.LogoPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ShutDownCheckBox As System.Windows.Forms.CheckBox
+    Friend WithEvents RestartCheckBox As System.Windows.Forms.CheckBox
+    Friend WithEvents SleepCheckBox As System.Windows.Forms.CheckBox
+    Friend WithEvents SwitchUserCheckBox As System.Windows.Forms.CheckBox
+    Friend WithEvents LockCheckBox As System.Windows.Forms.CheckBox
+    Friend WithEvents LogOffCheckBox As System.Windows.Forms.CheckBox
+    Friend WithEvents LogoPictureBox As System.Windows.Forms.PictureBox
+    Friend WithEvents AuthorLinkLabel As System.Windows.Forms.LinkLabel
+    Friend WithEvents AuthorLabel As System.Windows.Forms.Label
+    Friend WithEvents ChooseItemsLabel As System.Windows.Forms.Label
+    Friend WithEvents RestartExplorerCheckBox As System.Windows.Forms.CheckBox
+
+End Class
---
+++ JumpControl/Main.vb
@@ -0,0 +1,129 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class MainForm
+#Region "API Delarations"
+    Public Delegate Function CallBack(ByVal hWnd As IntPtr, ByVal lParam As Integer) As Boolean
+    <DllImport("wtsapi32.dll", SetLastError:=True)> _
+    Private Shared Function WTSDisconnectSession(ByVal hServer As IntPtr, ByVal sessionId As Integer, ByVal bWait As Boolean) As Boolean
+    End Function
+#End Region
+    Private ConfigXML As New XmlDocument
+    Private Jumplist As JumpList
+    Private ShutDownLink As New JumpListLink("shutdown.exe", "Shutdown")
+    Private RestartLink As New JumpListLink("shutdown.exe", "Restart")
+    Private SleepLink As New JumpListLink("rundll32.exe", "Sleep")
+    Private SwitchUserLink As New JumpListLink(Application.ExecutablePath, "Switch User")
+    Private LockLink As New JumpListLink("rundll32.exe", "Lock")
+    Private LogOffLink As New JumpListLink("shutdown.exe", "Log Off")
+    Private RestartExplorerLink As New JumpListLink("wmic.exe", "Restart Explorer")
+
+    Public Shared Sub Main(ByVal args() As String)
+        If args.Length = 1 Then
+            For Each arg In args
+                Select Case arg
+                    Case "-d"
+                        WTSDisconnectSession(0, -1, 0)
+                End Select
+            Next
+        Else
+            Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+
+            If TaskbarManager.IsPlatformSupported Then
+                Application.Run(New MainForm)
+            Else
+                MessageBox.Show("JumpControl requires Windows 7 to run.", "JumpControl", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        End If
+    End Sub
+
+    Private Sub Form_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'Load the config file
+        ConfigXML.Load("config.xml")
+        'Parse the config file
+        'Get item strings
+        ChooseItemsLabel.Text = ConfigXML.SelectSingleNode("/settings/chooseItems/string").InnerText & ":"
+        ShutDownCheckBox.Text = ConfigXML.SelectSingleNode("/settings/shutdown/string").InnerText
+        RestartCheckBox.Text = ConfigXML.SelectSingleNode("/settings/restart/string").InnerText
+        SleepCheckBox.Text = ConfigXML.SelectSingleNode("/settings/sleep/string").InnerText
+        SwitchUserCheckBox.Text = ConfigXML.SelectSingleNode("/settings/switchUser/string").InnerText
+        LockCheckBox.Text = ConfigXML.SelectSingleNode("/settings/lock/string").InnerText
+        LogOffCheckBox.Text = ConfigXML.SelectSingleNode("/settings/logOff/string").InnerText
+        RestartExplorerCheckBox.Text = ConfigXML.SelectSingleNode("/settings/restartExplorer/string").InnerText
+
+        'Check enable items
+        ShutDownCheckBox.Checked = ConfigXML.SelectSingleNode("/settings/shutdown/enabled").InnerText
+        RestartCheckBox.Checked = ConfigXML.SelectSingleNode("/settings/restart/enabled").InnerText
+        SleepCheckBox.Checked = ConfigXML.SelectSingleNode("/settings/sleep/enabled").InnerText
+        SwitchUserCheckBox.Checked = ConfigXML.SelectSingleNode("/settings/switchUser/enabled").InnerText
+        LockCheckBox.Checked = ConfigXML.SelectSingleNode("/settings/lock/enabled").InnerText
+        LogOffCheckBox.Checked = ConfigXML.SelectSingleNode("/settings/logOff/enabled").InnerText
+        RestartExplorerCheckBox.Checked = ConfigXML.SelectSingleNode("/settings/restartExplorer/enabled").InnerText
+    End Sub
+
+    Private Sub Form_Shown(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Shown
+        'Create the JumpList.
+        Jumplist = Jumplist.CreateJumpList()
+
+        'Get Jumplist task strings
+        ShutDownLink.Title = ConfigXML.SelectSingleNode("/settings/shutdown/string").InnerText
+        RestartLink.Title = ConfigXML.SelectSingleNode("/settings/restart/string").InnerText
+        SleepLink.Title = ConfigXML.SelectSingleNode("/settings/sleep/string").InnerText
+        SwitchUserLink.Title = ConfigXML.SelectSingleNode("/settings/switchUser/string").InnerText
+        LockLink.Title = ConfigXML.SelectSingleNode("/settings/lock/string").InnerText
+        LogOffLink.Title = ConfigXML.SelectSingleNode("/settings/logOff/string").InnerText
+        RestartExplorerLink.Title = ConfigXML.SelectSingleNode("/settings/restartExplorer/string").InnerText
+
+        'Set up the JumpList task arguments
+        ShutDownLink.Arguments = "-s -t 0"
+        RestartLink.Arguments = "-r -t 0"
+        SleepLink.Arguments = "powrprof.dll,SetSuspendState Sleep"
+        SwitchUserLink.Arguments = "-d"
+        LockLink.Arguments = "user32.dll,LockWorkStation"
+        LogOffLink.Arguments = "-l"""
+        RestartExplorerLink.Arguments = "process where name=""explorer.exe"" call terminate"
+
+        'Add the JumpList tasks
+        AddJumpListUserTasks()
+
+        'Add events to update the JumpList when the value of each checkbox is modified
+        AddHandler ShutDownCheckBox.CheckedChanged, AddressOf AddJumpListUserTasks
+        AddHandler RestartCheckBox.CheckedChanged, AddressOf AddJumpListUserTasks
+        AddHandler SleepCheckBox.CheckedChanged, AddressOf AddJumpListUserTasks
+        AddHandler SwitchUserCheckBox.CheckedChanged, AddressOf AddJumpListUserTasks
+        AddHandler LockCheckBox.CheckedChanged, AddressOf AddJumpListUserTasks
+        AddHandler LogOffCheckBox.CheckedChanged, AddressOf AddJumpListUserTasks
+        AddHandler RestartExplorerCheckBox.CheckedChanged, AddressOf AddJumpListUserTasks
+    End Sub
+
+    Private Sub Form_Closed(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles MyBase.FormClosed
+        ConfigXML.SelectSingleNode("/settings/shutdown/enabled").InnerText = ShutDownCheckBox.Checked
+        ConfigXML.SelectSingleNode("/settings/restart/enabled").InnerText = RestartCheckBox.Checked
+        ConfigXML.SelectSingleNode("/settings/sleep/enabled").InnerText = SleepCheckBox.Checked
+        ConfigXML.SelectSingleNode("/settings/switchUser/enabled").InnerText = SwitchUserCheckBox.Checked
+        ConfigXML.SelectSingleNode("/settings/lock/enabled").InnerText = LockCheckBox.Checked
+        ConfigXML.SelectSingleNode("/settings/logOff/enabled").InnerText = LogOffCheckBox.Checked
+        ConfigXML.SelectSingleNode("/settings/restartExplorer/enabled").InnerText = RestartExplorerCheckBox.Checked
+        ConfigXML.Save("config.xml")
+    End Sub
+
+    Private Sub GoToAuthorPage(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles AuthorLinkLabel.LinkClicked
+        Process.Start("http://protosphere.deviantart.com/")
+    End Sub
+
+    Private Sub AddJumpListUserTasks()
+        Jumplist.ClearAllUserTasks()
+
+        If ShutDownCheckBox.Checked Then Jumplist.AddUserTasks(ShutDownLink)
+        If RestartCheckBox.Checked Then Jumplist.AddUserTasks(RestartLink)
+        If SleepCheckBox.Checked Then Jumplist.AddUserTasks(SleepLink)
+        If SwitchUserCheckBox.Checked Or LockCheckBox.Checked Or LogOffCheckBox.Checked Then Jumplist.AddUserTasks(New JumpListSeparator)
+        If SwitchUserCheckBox.Checked Then Jumplist.AddUserTasks(SwitchUserLink)
+        If LockCheckBox.Checked Then Jumplist.AddUserTasks(LockLink)
+        If LogOffCheckBox.Checked Then Jumplist.AddUserTasks(LogOffLink)
+        If RestartExplorerCheckBox.Checked Then Jumplist.AddUserTasks(New JumpListSeparator)
+        If RestartExplorerCheckBox.Checked Then Jumplist.AddUserTasks(RestartExplorerLink)
+
+        Jumplist.Refresh()
+    End Sub
+End Class
